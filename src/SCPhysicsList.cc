1d3e101eb7c32b269d4c3da9c3118951d0c165ad
oviraptor <DIGI.I4ZJZD87@PM.ME>
2026-07-14 16:40:41 +0300

    Updated physics list

--- src/SCPhysicsList.cc
+++ src/SCPhysicsList.cc
@@ -1,9 +1,12 @@
 #include "SCPhysicsList.hh"
-#include "G4SystemOfUnits.hh"
+#include "G4EmStandardPhysics_option4.hh"
+#include "G4ThermalNeutrons.hh"
 
 SCPhysicsList::SCPhysicsList()
-    : FTFP_BERT_HP()   // call base constructor
+    : FTFP_BERT_HP()
 {
+    ReplacePhysics(new G4EmStandardPhysics_option4());
+    RegisterPhysics(new G4ThermalNeutrons());
 }
 
 SCPhysicsList::~SCPhysicsList() = default;
